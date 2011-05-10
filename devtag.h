649bf670a012558393d7c41f3660d0a017edd982
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2011-05-10 22:37:35 +0200

    clarify licence information (GPLv3 or later)

--- devtag.h
+++ devtag.h
@@ -1,6 +1,7 @@
 /* Copyright 2010 Stefan Tomanek <stefan.tomanek+th@wertarbyte.de>
  * You have permission to copy, modify, and redistribute under the
- * terms of the GPL. For full license terms, see COPYING.
+ * terms of the GPLv3 or any later version.
+ * For full license terms, see COPYING.
  */
 
 #define TH_DEVICE_TAG_LENGTH 256
