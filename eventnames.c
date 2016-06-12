b32787b61359b34519b32353d040b69bb776fa09
Stefan Tomanek <stefan.tomanek@wertarbyte.de>
2016-06-12 12:29:58 +0200

    simplify event parsing code

--- eventnames.c
+++ eventnames.c
@@ -11,16 +11,16 @@
 #include "eventtable.h"
 
 const char *lookup_event_name_i( const int evtype, const int evcode ) {
-	if (evtype == EV_KEY) {
-		return (KEY_MAX >= evcode ? KEY_NAME[ evcode ] : NULL);
+	switch(evtype) {
+		case EV_KEY:
+			return (KEY_MAX >= evcode ? KEY_NAME[ evcode ] : NULL);
+		case EV_SW:
+			return (SW_MAX >= evcode ? SW_NAME[ evcode ] : NULL);
+		case EV_REL:
+			return (REL_MAX >= evcode ? REL_NAME[ evcode ] : NULL);
+		default:
+			return NULL;
 	}
-	if (evtype == EV_SW) {
-		return (SW_MAX >= evcode ? SW_NAME[ evcode ] : NULL);
-	}
-	if (evtype == EV_REL) {
-		return (REL_MAX >= evcode ? REL_NAME[ evcode ] : NULL);
-	}
-	return NULL;
 }
 
 const char *lookup_event_name( const struct input_event ev ) {
@@ -35,27 +35,11 @@ const char *lookup_type_name( const struct input_event ev ) {
 	return lookup_type_name_i( ev.type );
 }
 
-int lookup_event_code( const char *eventname ) {
+static int lookup_index(const char **map, const char *value, int max) {
 	int i = 0;
-	while (i < KEY_MAX) {
-		if (KEY_NAME[i] != NULL &&
-		    strcmp(eventname, KEY_NAME[i]) == 0) {
-			return i;
-		}
-		i++;
-	}
-	i = 0;
-	while (i<SW_MAX) {
-		if (SW_NAME[i] != NULL &&
-		    strcmp(eventname, SW_NAME[i]) == 0) {
-			return i;
-		}
-		i++;
-	}
-	i = 0;
-	while (i<REL_MAX) {
-		if (REL_NAME[i] != NULL &&
-		    strcmp(eventname, REL_NAME[i]) == 0) {
+	while (i < max) {
+		if (map[i] != NULL &&
+		    strcmp(value, map[i])  == 0) {
 			return i;
 		}
 		i++;
@@ -63,29 +47,23 @@ int lookup_event_code( const char *eventname ) {
 	return -1;
 }
 
+int lookup_event_code( const char *eventname ) {
+	int res = -1;
+	if (res == -1) res = lookup_index(KEY_NAME, eventname, KEY_MAX);
+	if (res == -1) res = lookup_index(SW_NAME, eventname, SW_MAX);
+	if (res == -1) res = lookup_index(REL_NAME, eventname, REL_MAX);
+	return res;
+}
+
 int lookup_event_type( const char *eventname ) {
-	int i = 0;
-	while (i<KEY_MAX) {
-		if (KEY_NAME[i] != NULL &&
-		    strcmp(eventname, KEY_NAME[i]) == 0) {
-			return EV_KEY;
-		}
-		i++;
+	if (lookup_index(KEY_NAME, eventname, KEY_MAX) != -1) {
+		return EV_KEY;
 	}
-	i = 0;
-	while (i<SW_MAX) {
-		if (SW_NAME[i] != NULL &&
-		    strcmp(eventname, SW_NAME[i]) == 0) {
-			return EV_SW;
-		}
-		i++;
+	if (lookup_index(SW_NAME, eventname, SW_MAX) != -1) {
+		return EV_SW;
 	}
-	while (i<REL_MAX) {
-		if (REL_NAME[i] != NULL &&
-		    strcmp(eventname, REL_NAME[i]) == 0) {
-			return EV_SW;
-		}
-		i++;
+	if (lookup_index(REL_NAME, eventname, REL_MAX) != -1) {
+		return EV_REL;
 	}
 	return -1;
 }
